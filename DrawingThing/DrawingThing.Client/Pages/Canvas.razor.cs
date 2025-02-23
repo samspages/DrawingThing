8929be897fb31432c88e541e9c6e763aa82c8ff6
samspages <pagesh@dukes.jmu.edu>
2025-02-22 22:43:49 -0500

    init - cleanup and canvas page added

---
+++ DrawingThing/DrawingThing.Client/Pages/Canvas.razor.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace DrawingThing.Client.Pages
+{
+    public partial class Canvas
+    {
+        [Inject]
+        public IJSRuntime JS { get; set; }
+
+        protected override async void OnAfterRender(bool firstRender)
+        {
+            if (firstRender)
+            {
+                await JS.InvokeVoidAsync("Init");
+            }
+        }
+
+        public async Task ClearCanvas()
+        {
+            await JS.InvokeVoidAsync("ClearCanvas");
+        }
+    }
+}
